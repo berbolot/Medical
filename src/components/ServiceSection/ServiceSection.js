2315d8a7886545af75a1d0910186099132f031cf
berbolot <berbolotd@gmail.com>
2023-08-19 20:06:27 +0600

    first

--- src/components/ServiceSection/ServiceSection.js
+++ src/components/ServiceSection/ServiceSection.js
@@ -1,7 +1,3 @@
-import Service from "../Service/Service";
-import BitsToImage from "../BitsImage/BitsImage";
-import { setService } from "../../firebase/firebaseFuncs";
-import { useState } from "react";
 import sn1 from "../../images/1.png";
 import sn2 from "../../images/2.png";
 import sn3 from "../../images/3.png";
@@ -10,46 +6,6 @@ import sn5 from "../../images/5.png";
 import { Link } from "react-router-dom";
 
 const ServiceSection = () => {
-  const [bits, setBits] = useState("");
-
-  const handleFileChange = async (event) => {
-    const file = event.target.files[0];
-
-    try {
-      setService();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const fileToBits = (file) => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-
-      reader.onload = () => {
-        const buffer = reader.result;
-        const byteArray = new Uint8Array(buffer);
-
-        let bits = "";
-        for (let i = 0; i < byteArray.length; i++) {
-          const byte = byteArray[i];
-          const byteBits = byte.toString(2).padStart(8, "0");
-          bits += byteBits;
-        }
-
-        resolve(bits);
-      };
-
-      reader.onerror = () => {
-        reject(new Error("Failed to read the file."));
-      };
-
-      reader.readAsArrayBuffer(file);
-    });
-  };
-
-  setService();
-
   return (
     <div className="service-section" style={{ marginTop: "60px" }}>
       <div className="container">
@@ -67,27 +23,41 @@ const ServiceSection = () => {
         <div className="uslugi_df">
           <div className="uslugi">
             <img src={sn1} alt="images-KT" />
-           <Link to='/contact'><button className="service_btn">КТ</button></Link> 
+            <Link to="/contact">
+              <button className="service_btn">КТ</button>
+            </Link>
           </div>
           <div className="uslugi">
             <img src={sn2} alt="images-Лабораторная_диагностика" />
-           <Link to='/contact'><button className="service_btn">Лабораторная диагностика</button></Link>
+            <Link to="/contact">
+              <button className="service_btn">Лабораторная диагностика</button>
+            </Link>
           </div>
           <div className="uslugi">
             <img src={sn3} alt="images-УЗИ" />
-            <Link to='/contact'><button className="service_btn">УЗИ</button></Link>   
+            <Link to="/contact">
+              <button className="service_btn">УЗИ</button>
+            </Link>
           </div>
           <div className="uslugi">
             <img src={sn4} alt="images-Функциональная_диагностика" />
-            <Link to='/contact'><button className="service_btn">Функциональная диагностика</button></Link>  
+            <Link to="/contact">
+              <button className="service_btn">
+                Функциональная диагностика
+              </button>
+            </Link>
           </div>
           <div className="uslugi">
             <img src={sn5} alt="images-Эндоскопия" />
-            <Link to='/contact'><button className="service_btn">Эндоскопия</button></Link>   
+            <Link to="/contact">
+              <button className="service_btn">Эндоскопия</button>
+            </Link>
           </div>
           <div className="uslugi">
             <img src={sn5} alt="images-Эндоскопия" />
-            <Link to='/contact'><button className="service_btn">Эндоскопия</button></Link>   
+            <Link to="/contact">
+              <button className="service_btn">Эндоскопия</button>
+            </Link>
           </div>
         </div>
       </div>
